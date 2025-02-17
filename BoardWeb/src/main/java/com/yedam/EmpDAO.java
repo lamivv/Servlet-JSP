78922a2d63764503eb0be8035e6510787f77694d
lamivv <lamiv.v7@gmail.com>
2025-02-17 17:58:03 +0900

    250217

---
+++ BoardWeb/src/main/java/com/yedam/EmpDAO.java
@@ -0,0 +1,110 @@
+package com.yedam;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpDAO {
+	// Connection 객체
+	Connection getConnect() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 오라클DB의 접속정보 // xe는 접속할 데이터베이스 이름
+		String user = "hr";
+		String password = "hr";
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	} // end of Connection getConnect()
+	
+	// 상세조회
+	public Employee selectEmp(int empNo) {
+		String query = "select * from tbl_employees " //
+				+ "where emp_no = ?";
+		try {
+			PreparedStatement stmt = getConnect().prepareStatement(query);
+			stmt.setInt(1, empNo); // 첫번째 ?에 매개값으로 들어온 empNo를 넣음
+			
+			ResultSet rs = stmt.executeQuery(); // 조회
+			if(rs.next()) { // 조회결과가 한 건 있으면 true
+				Employee emp = new Employee();
+				emp.setEmpNo(rs.getInt("emp_no")); // 칼럼값
+				emp.setEmpName(rs.getString("emp_name"));
+				emp.setTelNo(rs.getString("tel_no"));
+				emp.setHireDate(rs.getDate("hire_date"));
+				emp.setSalary(rs.getInt("salary"));
+				
+				return emp; // Employee타입의 emp 반환
+			} 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null; // 조회된 결과가 없음 -> 매개값(사원번호)가 일치한 db가 없을 때
+	}
+	
+	
+	
+	// 등록
+	public boolean registerEmp(Employee emp) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String query = "insert into tbl_employees ";
+		query += "values (" + emp.getEmpNo() //
+				+ ", '" + emp.getEmpName() //
+				+ "', '" + emp.getTelNo() //
+				+ "', '" + sdf.format(emp.getHireDate()) //
+				+ "', " + emp.getSalary() + ")";
+		try {
+			Statement stmt = getConnect().createStatement();
+			int r = stmt.executeUpdate(query);
+			if (r > 0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return false; // 등록
+	} // end of registerEmp()
+	
+	// 목록
+	public List<Employee> search(Employee emp) { // 조회
+		List<Employee> empList = new ArrayList<>();
+		String qry = "select * from tbl_employees "
+//				+ "where emp_name = nvl('"+emp.getEmpName()+"', emp_name) "
+				+ "where emp_name = nvl(?, emp_name) " // number, carchar2 아무거나 들어오면 알아서 처리
+				+ "order by emp_no";
+		System.out.println(qry);
+		try {
+//			Statement stmt = getConnect().createStatement();
+			PreparedStatement stmt = getConnect().prepareStatement(qry);
+			stmt.setString(1, emp.getEmpName()); // 첫번째 ?에 사원이름을 대입
+//			stmt.setInt(1, emp.getEmpNo()); // 첫번째 ?에 사원번호를 대입
+//			ResultSet rs = stmt.executeQuery(qry);
+			ResultSet rs = stmt.executeQuery();
+			// 조회결과
+			while (rs.next()) {
+				Employee empl = new Employee();
+				empl.setEmpNo(rs.getInt("emp_no"));
+				empl.setEmpName(rs.getString("emp_name"));
+				empl.setTelNo(rs.getString("tel_no"));
+				empl.setHireDate(rs.getDate("hire_date"));
+				empl.setSalary(rs.getInt("salary"));
+
+				empList.add(empl);
+			} 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return empList;
+	}
+	
+}
---
+++ BoardWeb/src/main/java/com/yedam/addEmpServ.java
@@ -0,0 +1,33 @@
+package com.yedam;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// init - service - destroy : 서블릿의 생명주기
+@WebServlet("/addEmpServlet")
+public class addEmpServ extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8"); // 한글타입 표현가능해짐
+		String eno = req.getParameter("empNo"); // input name : empNo의 param에 담겨있는 값을 반환
+		String ename = req.getParameter("empName");
+		String tel = req.getParameter("telNo");
+
+		// db등록
+		EmpDAO edao = new EmpDAO();
+		boolean result = edao.registerEmp(new Employee(Integer.parseInt(eno), ename, tel));
+		if (result) {
+			resp.sendRedirect("sample"); // addEmpServlet -> sample 페이지이동 // 목록보여주는 페이지
+		} else {
+			resp.getWriter().print("처리실패");
+		}
+	}
+}
